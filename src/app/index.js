3d0c0fde8d8defa84d48feba5860344ced71df12
valmtv <valerij.matviiv@gmail.com>
2021-02-21 15:09:52 +0200

    master - use state, add button

--- src/app/index.js
+++ src/app/index.js
@@ -1,18 +1,22 @@
 // @flow
-import React from 'react';
+import React, { useState } from 'react';
 
 const App = () => {
+  const [value, setValue] = useState(0);
+
   const number1 = Math.floor(Math.random() * 10);
   const number2 = Math.floor(Math.random() * 10);
 
-  const handleInput = e => {
-    const userInput = e.currentTarget.value;
-    console.log('========', userInput);
+  const handleInput = e => setValue(e.currentTarget.value);
+
+  const checkAnswer = () => {
+    console.log('checking', value);
   };
 
   return <div>
     {number1} x {number2}
-    <input type="number" onChange={handleInput} />
+    <input onChange={handleInput} type="number" />
+    <button onClick={checkAnswer}>Check</button>
   </div>;
 };
 
