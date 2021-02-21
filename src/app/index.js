5e4617dc292d41ddebc1326a29831a0408e8d23c
valmtv <valerij.matviiv@gmail.com>
2021-02-21 16:48:26 +0200

    master - history

--- src/app/index.js
+++ src/app/index.js
@@ -6,20 +6,41 @@ const App = () => {
   const [num1, setNum1] = useState(Math.floor(Math.random() * 10));
   const [num2, setNum2] = useState(Math.floor(Math.random() * 10));
   const [correct, setCorrect] = useState(false);
+  const [answers, setAnswers] = useState([
+    // { num1: 12, num2: 45, answer: 34, correct: false },
+  ]);
 
   const handleInput = e => setAnswer(e.currentTarget.value);
 
   const checkAnswer = () => {
-    const Answer = num1 * num2;
-    if (parseInt(answer) === Answer) { setCorrect(true); }
+
+    // chck current answer
+    const isCorrect = parseInt(answer) === num1 * num2;
+    if (isCorrect) { setCorrect(true); }
     else { setCorrect(false); }
+
+    // add current answer to the answers array
+    setAnswers([
+      ...answers,
+      { num1: num1, num2: num2, answer: answer, correct: isCorrect },
+    ])
+    
+    // generate new problem
+    setAnswer(0);
+    setNum1(Math.floor(Math.random() * 10));
+    setNum2(Math.floor(Math.random() * 10));
+    setCorrect(false);
   };
 
   return <div>
     {num1} x {num2}
-    <input onChange={handleInput} type="number" />
+    <input onChange={handleInput} type="number" value={answer} />
     <button onClick={checkAnswer}>Check</button>
-    {correct ? 'correct' : 'incorrect'}
+    {answers.map((answr, idx) => (
+      <div key={idx}>
+        {answr.num1}x{answr.num2}={answr.answer} ({answr.correct ? 'correct' : 'incorrect'})
+      </div>
+    ))}
   </div>;
 };
 
