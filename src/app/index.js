79559cf27e5d024e336e7b98f99c782b86f797aa
valmtv <valerij.matviiv@gmail.com>
2021-03-06 14:06:50 +0200

    master - add minus exercises

--- src/app/index.js
+++ src/app/index.js
@@ -4,17 +4,24 @@ import React, { useState } from 'react';
 const App = () => {
   const [multiAnswer, setMultiAnswer] = useState('');
   const [plusAnswer, setPlusAnswer] = useState('');
+  const [minusAnswer, setMinusAnswer] = useState('');
   const [num1, setNum1] = useState(Math.floor(Math.random() * 10));
   const [num2, setNum2] = useState(Math.floor(Math.random() * 10));
   const [num3, setNum3] = useState(Math.floor(Math.random() * 1000));
   const [num4, setNum4] = useState(Math.floor(Math.random() * 1000));
+  const [num5, setNum5] = useState(Math.floor(Math.random() * 1000));
+  const [num6, setNum6] = useState(Math.floor(Math.random() * 1000));
   const [correct, setCorrect] = useState(false);
   const [multiAnswers, setMultiAnswers] = useState([]);
   const [plusAnswers, setPlusAnswers] = useState([]);
+  const [minusAnswers, setMinusAnswers] = useState([]);
   const [correctAmount, setCorrectAmount] = useState(0);
 
   const handleMultiInput = e => setMultiAnswer(e.currentTarget.value);
-  const handlePlusInput = e => setPlusAnswer(e.currentTarget.value);
+  const handlePlusInput = e => setPlusAnswer(e.currentTarget.value); 
+  const handleMinusInput = e => setMinusAnswer(e.currentTarget.value);
+
+//  const whatBigger = () = => {};
 
   const checkMultiAnswer = () => {
 
@@ -38,26 +45,40 @@ const App = () => {
 
   const checkPlusAnswer = () => {
 
-    // chck current answer
     const isCorrect = parseInt(plusAnswer) === num3 + num4;
     if (isCorrect) { setCorrect(true); setCorrectAmount(correctAmount + 1); }
     else { setCorrect(false); }
 
-    // add current answer to the answers array
     setPlusAnswers([
       ...plusAnswers,
       { num1: num3, num2: num4, answer: plusAnswer, correct: isCorrect },
     ])
     
-    // generate new problem
     setPlusAnswer('');
     setNum3(Math.floor(Math.random() * 1000));
     setNum4(Math.floor(Math.random() * 1000));
     setCorrect(false);
   };
 
+  const checkMinusAnswer = () => {
+
+    const isCorrect = parseInt(minusAnswer) === num5 - num6;
+    if (isCorrect) { setCorrect(true); setCorrectAmount(correctAmount + 1); }
+    else { setCorrect(false); }
+
+    setMinusAnswers([
+      ...minusAnswers,
+      { num1: num5, num2: num6, answer: minusAnswer, correct: isCorrect },
+    ])
+    
+    setMinusAnswer('');
+    setNum5(Math.floor(Math.random() * 1000));
+    setNum6(Math.floor(Math.random() * 1000));
+    setCorrect(false);
+  };
+
   return <div>
-    You complited correct {correctAmount}  of  {multiAnswers.length + plusAnswers.length}
+    You complited correct {correctAmount}  of  {multiAnswers.length + plusAnswers.length + minusAnswers.length}
     <br/>
      Multi : {num1} x {num2}
     <input onChange={handleMultiInput} type="number" value={multiAnswer} />
@@ -66,6 +87,11 @@ const App = () => {
     Plus : {num3} + {num4}
     <input onChange={handlePlusInput} type="number" value={plusAnswer} />
     <button onClick={checkPlusAnswer}>Check</button>
+    <br/>
+     Minus : {num5} - {num6}
+    <input onChange={handleMinusInput} type="number" value={minusAnswer} />
+    <button onClick={checkMinusAnswer}>Check</button>
+
     {multiAnswers.map((answr, idx) => (
       <div key={idx}>
         {answr.num1}x{answr.num2}={answr.answer} ({answr.correct ? 'correct' : 'incorrect'})
@@ -74,6 +100,11 @@ const App = () => {
     {plusAnswers.map((answr, idx) => (
       <div key={idx}>
         {answr.num1}+{answr.num2}={answr.answer} ({answr.correct ? 'correct' : 'incorrect'})
+      </div>
+    ))}
+    {minusAnswers.map((answr, idx) => (
+      <div key={idx}>
+        {answr.num1}-{answr.num2}={answr.answer} ({answr.correct ? 'correct' : 'incorrect'})
       </div>
     ))}
   </div>;
