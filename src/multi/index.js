6d197115c254b076d4eb0720dbbf8b3f2bcce560
valmtv <valerij.matviiv@gmail.com>
2021-10-19 20:00:10 +0300

    master - made individual column for multi range and history

--- src/multi/index.js
+++ src/multi/index.js
@@ -6,6 +6,12 @@ const Input = styled.input`
   width: 75px;
 `;
 
+const Div1 = styled.div`
+  display: flex;
+  flex-direction : row;
+  justify-content : space-between;
+`;
+
 const Multi = ({
   answers, setAnswers,
   // TODO: remouse reduuce to calc how maybe correct answers are in
@@ -19,9 +25,12 @@ const Multi = ({
     setInternalAnswers(answers);
   }, [answers]);
 
+
+  const [minMulti, setMinMulti] = useState(1);
+  const [maxMulti, setMaxMulti] = useState(10);
   const [multiAnswer, setMultiAnswer] = useState('');
-  const [num1, setNum1] = useState(randomNumber(1, 10));
-  const [num2, setNum2] = useState(randomNumber(1, 10));
+  const [num1, setNum1] = useState(randomNumber(minMulti, maxMulti));
+  const [num2, setNum2] = useState(randomNumber(minMulti, maxMulti));
   const [correct, setCorrect] = useState(false);
 
   const handleMultiInput = e => setMultiAnswer(e.currentTarget.value);
@@ -62,28 +71,33 @@ const Multi = ({
     setCorrect(false);
   };
 
-  return <>
-    Multi :
-    <br/>
-    Correct__ {correctAmount} / {internalAnswers.length}
-    <br/>
-    Example : {num1} x {num2}
+  return <Div1>
     <div>
-      <Input
-        onChange={handleMultiInput}
-        type="number"
-        value={multiAnswer}
-      />
-      <button onClick={checkMultiAnswer}>Check</button>
-    </div>
-      {internalAnswers.map((answr, idx) => (
-      <div key={idx}>
-        {answr.num1}x{answr.num2}={answr.answer} ({
-          answr.correct ? 'correct' : 'incorrect'
-        })
+      Multi :
+      <br/>
+      Correct__ {correctAmount} / {internalAnswers.length}
+      <br/>
+      Example : {num1} x {num2}
+      <div>
+        <Input
+          onChange={handleMultiInput}
+          type="number"
+          value={multiAnswer}
+        />
+        <button onClick={checkMultiAnswer}>Check</button>
       </div>
-    ))}
-  </>;
+    </div>
+    <div>
+      Multi numbers range is from {minMulti} to {maxMulti}
+        {internalAnswers.map((answr, idx) => (
+        <div key={idx}>
+          {answr.num1}x{answr.num2}={answr.answer} ({
+            answr.correct ? 'correct' : 'incorrect'
+          })
+        </div>
+      ))}
+    </div>
+  </Div1>
 };
 
 export default Multi;
