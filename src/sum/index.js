364de217d77541450e8c7ba24d9238cd46798e86
valmtv <valerij.matviiv@gmail.com>
2021-11-21 16:13:46 +0200

    valera  make plus folding

--- src/sum/index.js
+++ src/sum/index.js
@@ -12,6 +12,19 @@ const Div1 = styled.div`
   justify-content: space-between;
 `;
 
+const PlusButton = styled.div`
+  cursor: pointer;
+  border-radius: 30px;
+  background-color: green;
+  height: 150px;
+  width: 150px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin: 15px;
+  font-size: 30px;
+`;
+
 const Sum = ({}) => { 
   const [plusAnswers, setPlusAnswers] = useState([]);
   const [correctPlusAmount, setCorrectPlusAmount] = useState(0);
@@ -21,8 +34,10 @@ const Sum = ({}) => {
   const [num3, setNum3] = useState(randomNumber(minSum, maxSum));
   const [num4, setNum4] = useState(randomNumber(minSum, maxSum));
   const [correct, setCorrect] = useState(false);
+  const [isPlus, setIsPlus] = useState(false);
 
   const handlePlusInput = e => setPlusAnswer(e.currentTarget.value); 
+  const isPlusChange = () => setIsPlus(!isPlus);
 
   const checkPlusAnswer = () => {
     const isCorrect = parseInt(plusAnswer) === num3 + num4;
@@ -55,33 +70,41 @@ const Sum = ({}) => {
     setCorrect(false);
   };
 
-  return <Div1>
-    <div>
-      Plus : 
-      <br/>
-      Correct__ {correctPlusAmount} / {plusAnswers.length}
-      <br/> 
-      Example : {num3} + {num4}
-      <div>
-        <Input 
-          onChange={handlePlusInput}
-          type="number"
-          value={plusAnswer}
-        />
-        <button onClick={checkPlusAnswer}>Check</button>
-      </div>
-    </div>
-    <div>
-      Plus numbers range is from {minSum} to {maxSum}
-      {plusAnswers.map((answr, idx) => (
-        <div key={idx}>
-          {answr.num1}+{answr.num2}={answr.answer} ({
-            answr.correct ? 'correct' : 'incorrect'
-            })
+  if (isPlus) {
+    return <>
+      <PlusButton onClick={isPlusChange}>Plus</PlusButton>
+      <Div1>
+        <div>
+          Plus : 
+          <br/>
+          Correct__ {correctPlusAmount} / {plusAnswers.length}
+          <br/> 
+          Example : {num3} + {num4}
+          <div>
+            <Input 
+              onChange={handlePlusInput}
+              type="number"
+              value={plusAnswer}
+            />
+            <button onClick={checkPlusAnswer}>Check</button>
+          </div>
         </div>
-      ))}
-    </div>
-  </Div1>
+        <div>
+          Plus numbers range is from {minSum} to {maxSum}
+          {plusAnswers.map((answr, idx) => (
+            <div key={idx}>
+              {answr.num1}+{answr.num2}={answr.answer} ({
+                answr.correct ? 'correct' : 'incorrect'
+                })
+            </div>
+          ))}
+        </div>
+      </Div1>
+    </>
+  }
+  else {
+    return <PlusButton onClick={isPlusChange}>Plus</PlusButton>
+  };
 };
 
 export default Sum;
